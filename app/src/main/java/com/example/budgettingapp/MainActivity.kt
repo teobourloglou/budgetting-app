9f6e1a6280493cc11587d36c5e8d91343a3870b5
Theo2801 <theologos.bourloglou@gmail.com>
2024-04-20 01:12:53 +0300

    [feat] Continue home screen, add custom fonts

--- app/src/main/java/com/example/budgettingapp/MainActivity.kt
+++ app/src/main/java/com/example/budgettingapp/MainActivity.kt
@@ -6,15 +6,23 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FormatListBulleted
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -24,8 +32,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +53,7 @@ class MainActivity : ComponentActivity() {
             BudgettingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Teo", "Liza")
+                    Home(300, 0, modifier = Modifier)
                 }
             }
         }
@@ -48,35 +61,185 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
+fun Home(amount: Int, amountCents: Int, modifier: Modifier = Modifier) {
     Scaffold(
         topBar = {
             TopBar()
         }
     ) { innerPadding ->
         Column (
-            verticalArrangement = Arrangement.Bottom,
+            verticalArrangement = Arrangement.SpaceEvenly,
             modifier = modifier
                 .padding(8.dp)
                 .padding(innerPadding)
+                .fillMaxHeight()
         ) {
-            Text(
-                text = "Hello $name!",
-                fontSize = 100.sp,
-                textAlign = TextAlign.Center,
-                lineHeight = 116.sp,
+            var selected by remember { mutableIntStateOf(0)}
+            MoneyAmount(amount, amountCents, modifier = Modifier)
+            Column(
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 15.dp,
+                    alignment = Alignment.Bottom
+                ),
                 modifier = modifier
-            )
-            Text(
-                text = from,
-                textAlign = TextAlign.Right,
-                fontSize = 36.sp,
-            )
+                    .padding(8.dp)
+                    .padding(innerPadding)
+                    .weight(1f)
+            ) {
+                TimeSelector(
+                    modifier = modifier,
+                    selected = selected,
+                    onTimeSelected = { newSelected -> selected = newSelected }
+                )
+                ActionGroup()
+            }
         }
     }
 
 }
 
+@Composable
+fun MoneyAmount( amount: Int, amountCents: Int, modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 2.dp,
+            alignment = Alignment.CenterHorizontally
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 100.dp)
+    ) {
+        Text(
+            text = "$",
+            textAlign = TextAlign.Right,
+            fontSize = 36.sp,
+            color = MaterialTheme.colorScheme.outline,
+            modifier = Modifier.padding(top = 12.dp)
+        )
+        Text(
+            text = "$amount",
+            fontSize = 110.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 110.sp,
+            modifier = modifier
+        )
+        Text(
+            text = ".$amountCents",
+            textAlign = TextAlign.Right,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(top = 10.dp)
+        )
+    }
+}
+
+
+@Composable
+fun TimeSelector(modifier: Modifier = Modifier, selected : Int, onTimeSelected: (Int) -> Unit) {
+    Row (
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 10.dp,
+            alignment = Alignment.CenterHorizontally
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.secondary,
+                shape = RoundedCornerShape(30.dp)
+            )
+            .padding(horizontal = 5.dp)
+
+    ) {
+        Button(
+            onClick = { onTimeSelected(0) },
+            colors = if (selected == 0) ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary) else ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+            modifier = modifier.weight(1f)
+        ) {
+            Text(
+                stringResource(R.string.week),
+                color = if (selected == 0) MaterialTheme.colorScheme.onTertiary else MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        Button(
+            onClick = { onTimeSelected(1) },
+            colors = if (selected == 1) ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary) else ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+            modifier = modifier.weight(1f)
+        ) {
+            Text(
+                stringResource(R.string.month),
+                color = if (selected == 1) MaterialTheme.colorScheme.onTertiary else MaterialTheme.colorScheme.onSecondary
+            )
+        }
+        Button(
+            onClick = { onTimeSelected(2) },
+            colors = if (selected == 2) ButtonDefaults.buttonColors(MaterialTheme.colorScheme.tertiary) else ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+            modifier = modifier.weight(1f)
+        ) {
+            Text(
+                stringResource(R.string.year),
+                color = if (selected == 2) MaterialTheme.colorScheme.onTertiary else MaterialTheme.colorScheme.onSecondary
+            )
+        }
+    }
+}
+
+@Composable
+fun ActionGroup(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 10.dp,
+            alignment = Alignment.CenterHorizontally
+        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max)
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+            modifier = modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .aspectRatio(1f)
+        ) {
+            Text(text = "TO DO CHARTS")
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(
+                space = 15.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            modifier = modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .height(IntrinsicSize.Max)
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .weight(1f)
+            ) {
+                Text (
+                    text = stringResource(R.string.add_expense)
+                )
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .weight(1f)
+            ) {
+
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(modifier: Modifier = Modifier) {
     CenterAlignedTopAppBar(
@@ -98,7 +261,8 @@ fun TopBar(modifier: Modifier = Modifier) {
                     )
                 }
                 Text(
-                    text = "OVERVIEW",
+                    text = stringResource(R.string.overview),
+                    style = MaterialTheme.typography.bodyLarge,
                     color = MaterialTheme.colorScheme.outline,
                     fontSize = 16.sp
                 )
@@ -121,10 +285,10 @@ fun TopBar(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun GreetingPreview() {
+fun HomePreview() {
     BudgettingAppTheme {
         Surface (modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-            Greeting("Teo", "From Liza")
+            Home(300, 0)
         }
     }
 }
--- app/src/main/java/com/example/budgettingapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/budgettingapp/ui/theme/Type.kt
@@ -2,33 +2,46 @@ package com.example.budgettingapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.budgettingapp.R
+
+val Lettera = FontFamily(
+        Font(R.font.lettera_regular)
+)
+
+val NDot = FontFamily(
+        Font(R.font.ndot)
+)
+
+val NDot2 = FontFamily(
+        Font(R.font.ndot_2)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
         bodyLarge = TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = NDot2,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
                 lineHeight = 24.sp,
                 letterSpacing = 0.5.sp
+        ),
+        titleLarge = TextStyle(
+                fontFamily = Lettera,
+                fontWeight = FontWeight.Normal,
+                fontSize = 22.sp,
+                lineHeight = 28.sp,
+                letterSpacing = 0.sp
+        ),
+        labelSmall = TextStyle(
+                fontFamily = Lettera,
+                fontWeight = FontWeight.Medium,
+                fontSize = 11.sp,
+                lineHeight = 16.sp,
+                letterSpacing = 0.5.sp
         )
-        /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
